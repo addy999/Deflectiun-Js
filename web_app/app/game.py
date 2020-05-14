68b3fda2cca36e7aeb7a3143484f29ec4e7c2f6c
Addy Bhatia <addy@loftorbital.com>
2020-05-14 13:44:43 -0400

    reorg game for refresh

--- web_app/app/game.py
+++ web_app/app/game.py
@@ -53,10 +53,10 @@ def get_status(game):
     
     return data
 
-def step(game, cmd):
+def step(cmd):
     
-    won, fail, message = game.step(cmd)
-    status = get_status(game)
+    won, fail, message = _game.step(cmd)
+    status = get_status(_game)
     status.update({
         "won" : won,
         "fail" : fail,
@@ -64,4 +64,15 @@ def step(game, cmd):
     })
 
     return status
+
+screen_x, screen_y = 900, 700
+sc = Spacecraft('Test', mass = 100, thrust_force = 2000, gas_level = 500, width=35, length=35)
+
+planet = Planet('Test', mass = 3e16, orbit = Orbit(a=screen_x*500/1920, b=screen_y*500/1080, center_x=screen_x, center_y=screen_y/2, CW=True, angular_step = 2*np.pi/(200.0), progress = np.pi/2))
+
+planet2 = Planet('Test2', mass = 2e16, orbit = Orbit(a=screen_x*800/1920, b=screen_y*500/1080, center_x=200, center_y=screen_y/2, CW=False, angular_step = 2*np.pi/(200.0), progress = np.pi/4))
+
+scene = Scene((screen_x, screen_y),sc, [planet, planet2], win_region = ([0,screen_y], [screen_x, screen_y]), win_velocity = 90.0)
+builder = LevelBuilder(screen_x, screen_y)
+_game = Game(scenes=[builder.create("easy") for i in range(10)], fps=25)
     
