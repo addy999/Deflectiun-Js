7c68720711f6c792a0a09939a1e3ed4b35c61162
Addy Bhatia <addy@loftorbital.com>
2020-05-14 23:08:59 -0400

    add threading

--- web_app/app/__init__.py
+++ web_app/app/__init__.py
@@ -14,5 +14,5 @@ log = logging.getLogger('werkzeug')
 log.disabled = True
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=False, threaded=True, processes=2)
   # socketio.run(app)
