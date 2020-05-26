e9807ad514455e54e5adcccea974b2eec6f2313d
addy999 <addy@loftorbital.com>
2020-05-26 01:35:21 -0400

    mobile check added

--- web_app/app/static/js/main.js
+++ web_app/app/static/js/main.js
@@ -1,33 +1,13 @@
 $(document).ready(function () {
 
-    // var socket = io.connect('http://' + document.domain + ':' + location.port);
-    // socket.on('my response', function(msg) {
-    //    console.log(msg.data);
-    // });
-
     window.addEventListener("keydown", function(e) {
         if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
             e.preventDefault();
         }
     }, false);
 
-    // document.getElementById("buttons").children[1].style.display="none";
-    // document.getElementById("buttons").children[2].style.display="none";
+    if(mobileCheck()) {
+        document.getElementById("warning").style.display = "block";
+    }
 
 });
-
-// var interval = setInterval(update, 40);
-
-// function speedCheck() {
-//     MeasureConnectionSpeed();
-//     $.get( "/speedcheck" , (okay) => {
-//         okay = $.parseJSON(okay);
-//         if(okay) {
-//             overlay_off();
-//         }
-//         else {
-//             overlay_on();
-//         }
-//     })
-// }
-
